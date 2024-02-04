7693a379cb402088b77de558f8b4a03b93480ccc
juhui88 <sand8594@naver.com>
2024-02-04 12:25:46 +0900

    setup

---
+++ foodsaver/src/app/foodbank/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            푸드뱅크~
+        </div>
+    );
+};
+
+export default page;
--- foodsaver/tailwind.config.ts
+++ foodsaver/tailwind.config.ts
@@ -8,11 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        mainColor: "#4FB14F",
+        textColor: "#4B574B",
+        borderColor: "#DEDEDE"
+      }
     },
   },
   plugins: [],
