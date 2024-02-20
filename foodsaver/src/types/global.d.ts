1638b4a72d5ead83ebfefc680666c022df31ffa2
juhui88 <sand8594@naver.com>
2024-02-20 16:45:13 +0900

    fix:deploy error

--- foodsaver/src/types/global.d.ts
+++ foodsaver/src/types/global.d.ts
@@ -1,3 +1,11 @@
-interface Window {
-    google?: any;
+declare global {
+    interface Window {
+        google: {
+            maps: {
+                Map: any;
+                Marker: any;
+                // Add other types as needed
+            };
+        };
+    }
 }
